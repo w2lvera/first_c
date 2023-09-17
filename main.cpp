644d97d7e91298c941466c2672e77aafe6645290
vera <w2lvera@gmail.com>
2023-09-17 22:37:01 +0300

    references

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ void swap(int* a, int* b){
     *a = *b; //по адресу а положить то, что лежит по адресу b
     *b = help;//по адресу b положить то, что лежит и help
 }
+////////////////// ссылки появились в с++ ///////////
+void swap(int& a, int& b){
+    int help;
+    help = a;
+    a = b;
+    b = help;
+}
 int main()
 {
     int a = 123;
@@ -33,6 +40,13 @@ int main()
     printf("c = %d  b = %d\n",c,b);
     swap(&c,&b);// swap(&с - взятие адреса переменной с , &b - взятие адреса переменной b)
     printf("after swap \nc = %d  b = %d\n",c,b);
+//////////////// references ////////////////////////
+    int c1 = 100;
+    int b1 = 1;
+    printf("\n references \n");
+    printf("c1 = %d  b1 = %d\n",c1,b1);
+    swap(c1,b1);
+    printf("after swap \nc1 = %d  b1 = %d\n",c1,b1);
     return 0;
 }
  //git push --set-upstream fromQt master
