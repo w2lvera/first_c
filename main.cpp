d5f46c9854e2962707fc81fb89de0d4f364e9282
vera <w2lvera@gmail.com>
2023-09-17 22:07:35 +0300

    lesson2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-using namespace std;
 int lastDigit(int value){
     return value - value/10*10;
 }
@@ -9,6 +8,13 @@ int even(int value){
 }
 int check(int value, int del){
     return value - value/del*del;
+}
+/////////////////// указатели /////////////////
+void swap(int* a, int* b){
+    int help;
+    help = *a; // положить в а то, что лежит по адресу b
+    *a = *b; //по адресу а положить то, что лежит по адресу b
+    *b = help;//по адресу b положить то, что лежит и help
 }
 int main()
 {
@@ -20,7 +26,13 @@ int main()
     printf("result = %d\n",result);
     result = check(a,10);
     printf("result = %d\n",result);
-
+//////////////////////////// указатели /////////////////////////////
+    int c = 100;
+    int b = 1;
+    printf("\n pointers \n");
+    printf("c = %d  b = %d\n",c,b);
+    swap(&c,&b);// swap(&с - взятие адреса переменной с , &b - взятие адреса переменной b)
+    printf("after swap \nc = %d  b = %d\n",c,b);
     return 0;
 }
  //git push --set-upstream fromQt master
