2bd116bbf88b262cb77c03074c489f2a556d2bf1
vera <w2lvera@gmail.com>
2023-09-17 22:07:35 +0300

    lesson2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
+#include <stdio.h>
 
 using namespace std;
-
+int lastDigit(int value){
+    return value - value/10*10;
+}
+int even(int value){
+    return value - value/2*2;
+}
+int check(int value, int del){
+    return value - value/del*del;
+}
 int main()
 {
-    cout << "Hello World! change" << endl;
+    int a = 123;
+    int result;
+    result = even(a);
+    printf("result = %d\n",result);
+    result = lastDigit(a);
+    printf("result = %d\n",result);
+    result = check(a,10);
+    printf("result = %d\n",result);
+
     return 0;
 }
  //git push --set-upstream fromQt master
