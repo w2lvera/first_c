148a1943746bb85f61aafdad684b5041f815c3f9
vera <w2lvera@gmail.com>
2023-09-27 13:24:09 +0300

    использование операторов: условного (if and ?) цикла(while,for)

--- main.cpp
+++ main.cpp
@@ -1,52 +1,97 @@
 #include <stdio.h>
+//функция возвращает модуль числа
+int mod(int x) {
+    if (x < 0)return -x;
+    return x;
+}
+//функция возвращает модуль числа с использованием оператора ?
+int mod1(int x) {
+    return (x<0)?-x:x;
+}
+//функция возвращает максимальное из двух чисел
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+//функция возвращает максимальное из трех чисел
+int max(int a, int b, int c) {
+    int max=a;
+    if (b > max)max = b;
+    if (c > max)max = c;
+    return max;
+}
+//функция возвращает максимальное из n вводимых чисел
+int maximum(int n) {
+    printf("\ninput %d integers separated by spaces\n",n);
+    int max=0x80000000;//самое маленькое int
+   // int max = ~0x7ffffffff;
+    int i = 0;
+    int x;
+    while (i < n) {
+        scanf("%d", &x);
+        if (x > max)max = x;
+        i++;
+    }
+    return max;
+}
+int maximum1(int n) {
+    int max = 0x80000000;
+    // int max = ~0x7ffffffff;
 
-int lastDigit(int value){
-    return value - value/10*10;
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        if (x == 100)continue;//число 100 не проверяется на максимум
+        if (x > max)max = x;
+
+    }
+    return max;
 }
-int even(int value){
-    return value - value/2*2;
+//функция возвращает максимальное из трех чисел
+int max1(int a, int b, int c) {
+    int max = a;
+    max = (b > max) ? b : max;
+    max = (c > max) ? c : max;
+    return max;
 }
-int check(int value, int del){
-    return value - value/del*del;
+//функция выводит делители числа
+void printDel(int x) {
+    if (x <= 1)return ;
+    printf("\nделители числа %d\n",x);
+    for (int i = 2; i * i <= x; i++) {
+         if (x % i == 0)
+            printf("%d %d\n", x / i, i);
+    }
 }
-/////////////////// указатели /////////////////
-void swap(int* a, int* b){
-    int help;
-    help = *a; // положить в а то, что лежит по адресу b
-    *a = *b; //по адресу а положить то, что лежит по адресу b
-    *b = help;//по адресу b положить то, что лежит и help
+//функция проверяет простоту числа. 0 - непростое 1 - простое
+int prime(int x) {
+    if (x <= 1)return 0;
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0)  return 0;
+    return 1;
 }
-////////////////// ссылки появились в с++ ///////////
-void swap(int& a, int& b){
-    int help;
-    help = a;
-    a = b;
-    b = help;
+//функция возвращает простое число, ближайшее к данному х
+int prime_closestToThis(int x) {
+    int i;
+    for (i = 1; !prime(x + i) && !prime(x - i); i++);
+    if (prime(x + i))return x + i;
+    return x - i;
 }
 int main()
 {
-    int a = 123;
-    int result;
-    result = even(a);
-    printf("result = %d\n",result);
-    result = lastDigit(a);
-    printf("result = %d\n",result);
-    result = check(a,10);
-    printf("result = %d\n",result);
-//////////////////////////// указатели /////////////////////////////
-    int c = 100;
-    int b = 1;
-    printf("\n pointers \n");
-    printf("c = %d  b = %d\n",c,b);
-    swap(&c,&b);// swap(&с - взятие адреса переменной с , &b - взятие адреса переменной b)
-    printf("after swap \nc = %d  b = %d\n",c,b);
-//////////////// references ////////////////////////
-    int c1 = 100;
-    int b1 = 1;
-    printf("\n references \n");
-    printf("c1 = %d  b1 = %d\n",c1,b1);
-    swap(c1,b1);
-    printf("after swap \nc1 = %d  b1 = %d\n",c1,b1);
-    return 0;
+    int res;
+    res = mod(-4);
+    printf("\nmod(-4) = %d",res);
+    res = mod1(-4);
+    printf("\nmod(-4) = %d",res);
+    res = max(-4,5);
+    printf("\nmax(-4,5) = %d",res);
+    res = max(-4,-5,-2);
+    printf("\nmax(-4,-5,-2) = %d\n",res);
+    res = maximum(5);//функция возвращает максимальное из 5 вводимых чисел
+    printf("\nmaximum(5) = %d",res);
+    printDel(36);//функция выводит делители числа 36
+    res = prime_closestToThis(50);//функция возвращает простое число, ближайшее к 50
+    printf("\nprime_closestToThis(50) = %d",res);
 }
  //git push --set-upstream fromQt master
