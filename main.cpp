8a30ffd7d41373198f74a3281e434cb96e222006
w2lvera <w2lvera@gmail.com>
2023-10-16 10:16:09 +0300

    lesson5 витовые операции

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
+typedef unsigned char byte;
+//проверка к-го бита value
+byte checkByte(byte value,byte k){
+    return (value&(1<<k))>>k;
+}
 
 int main()
 {
-    cout << "Hello World! change" << endl;
+    byte x = 252;//двоичное представление 1111 1100
+    //mask = 1000 0000...0000 0000, k - 7...0 бит
+    for(int mask = 0x80,k=7;mask;mask=mask>>1,k--){
+        byte res = (x&mask)>>k;
+        printf("%d",res);
+    }
     return 0;
 }
  //git push --set-upstream fromQt master
