5433fd2d8cdfb5bfeddab9dee070922fdd9b7d3d
w2lvera <w2lvera@gmail.com>
2023-10-16 10:25:02 +0300

    lesson5 checking the kth bit of value

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 typedef unsigned char byte;
-//проверка к-го бита value
+//checking the k-th bit of value
+
 byte checkByte(byte value,byte k){
     return (value&(1<<k))>>k;
 }
 
 int main()
 {
-    byte x = 252;//двоичное представление 1111 1100
-    //mask = 1000 0000...0000 0000, k - 7...0 бит
+    byte x = 252;    //binary representation of x --> 1111 1100
+    //mask = 1000 0000...0000 0000, k - 7...0 bit
     for(int mask = 0x80,k=7;mask;mask=mask>>1,k--){
         byte res = (x&mask)>>k;
         printf("%d",res);
